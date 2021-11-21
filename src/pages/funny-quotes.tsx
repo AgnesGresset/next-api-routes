a5845fdd6942500923ad830fc60a99ea38a68594
Agnès Gresset <agnes.gresset@hotmail.fr>
2021-11-21 21:39:09 +0100

    [API-ROUTES] TODO: use shuffle array?

--- src/pages/funny-quotes.tsx
+++ src/pages/funny-quotes.tsx
@@ -5,12 +5,12 @@ import useFetchApi from './hooks/useFetchApi'
 
 const FunnyQuotes = () => {
 	const { fetchData, data, loading, error } = useFetchApi<FunnyQuoteProps[]>('funny-quotes')
+  const [ quote, setQuote ] = React.useState<FunnyQuoteProps | undefined>(undefined)
 
+  //TODO: do it differently, because it is not updating smootlhy every 5 seconds... shufflearray?
   React.useEffect(() => {
     fetchData()
   },[])
-
-  const [ quote, setQuote ] = React.useState<FunnyQuoteProps | undefined>(data ? data[0] : undefined)
 
   React.useEffect(() => {
     if (data) {
@@ -19,7 +19,7 @@ const FunnyQuotes = () => {
         setQuote(data[index])
       }, 5000)
     }
-    
+
     // clean up interval on unmount
     return () => {
       window.clearInterval()
